25b0aa684e0c54c1f51d7836a55def9b0d7e063a
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2023-03-14 00:57:02 +0530

    Made a few changes for the new Project 🤞

--- Frontend App with Solidity - Assessment/scripts/deploy.js
+++ Frontend App with Solidity - Assessment/scripts/deploy.js
@@ -1,17 +1,11 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
     await hre.run("compile");
-    const Banking = await hre.ethers.getContractFactory("Bank");
-    const banking = await Banking.deploy();
-    await banking.deployed();
-    console.log("CONTRACT ADDRESS: ", banking.address);
+    const Gameing = await hre.ethers.getContractFactory("Game");
+    const gameing = await Gameing.deploy();
+    await gameing.deployed();
+    console.log("CONTRACT ADDRESS: ", gameing.address);
 }
 
 main().catch((error) => {
